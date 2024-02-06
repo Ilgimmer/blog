5bfce0b573e23ec5f891cb18b5b105a9aa814897
Ilnaz Giniyatullin <ilgimmer@mail.com>
2024-02-06 20:53:41 +0300

    первая фиксация

--- src/blog/main.py
+++ src/blog/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from blog.resources import router
+
 
 def get_app():
     app = FastAPI()
 
+    app.include_router(router) # <- вот тут мы зарегистрировали роутер
+
     return app
+
 
 app = get_app()
